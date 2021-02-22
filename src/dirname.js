575fd8f78a16a9f1db5f1326cccd87051c3eccb2
Henrique Borges <henriqueborgeshbr@gmail.com>
2021-02-22 03:26:46 -0300

    refactor: rename dirname export to "__dirname"

--- src/dirname.js
+++ src/dirname.js
@@ -1,4 +1,4 @@
-import { dirname } from 'path'
+import path from 'path'
 import { fileURLToPath } from 'url'
 
-export const __dirname = url => dirname(fileURLToPath(url))
+export const dirname = url => path.dirname(fileURLToPath(url))
