36fb4009aa502b390789b22c6a138d19c0a042b5
Henrique Borges <henriqueborgeshbr@gmail.com>
2021-02-23 18:35:40 -0300

    fix: force push git tags
    Otherwise, this would cause and error when the tag in question already
    exists on the remote

--- src/push.js
+++ src/push.js
@@ -11,5 +11,5 @@ export const push = async ({ cwd, dryRun, noPush }) => {
 
   log(chalk`{blue Pushing release and tags}`)
   await execa('git', ['push'], { cwd })
-  await execa('git', ['push', '--tags'], { cwd })
+  await execa('git', ['push', '-f', '--tags'], { cwd })
 }
